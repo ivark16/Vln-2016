b693cc8275604074b4359ba01a6251a69596be9e
Ívar Örn Kristjánsson <var rn Kristjnsson>
2016-11-29 10:13:01 +0000

    Added private's to list

--- Vln-2016/list.h
+++ Vln-2016/list.h
@@ -1,11 +1,21 @@
 #ifndef LIST_H
 #define LIST_H
-
+#include <string>
+using namespace std;
 
 class list
 {
+private:
+    string name;
+    int birthYear;
+    int deathYear;
+    char sex;
+    int TuringAwards;
+
 public:
     list();
+
+
 };
 
 #endif // LIST_H
