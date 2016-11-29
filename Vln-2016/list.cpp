dbb666dca055c2408717ee121b734fbcc98ccb11
Ívar Örn Kristjánsson <var rn Kristjnsson>
2016-11-29 11:25:28 +0000

    Making readFile

--- Vln-2016/list.cpp
+++ Vln-2016/list.cpp
@@ -1,6 +1,37 @@
 #include "list.h"
+using namespace std;
 
 list::list()
 {
 
 }
+
+//Reading from file
+bool list::readFile ()
+{
+    fstream file;
+    file.open("turingAwardWinners.txt");
+
+
+    file >> _name;
+    file >> _sex;
+    file >> _birthYear;
+    file >> _deathYear;
+    file >> _TuringAwards;
+    if (file.eof())
+    {
+        return true;
+    }
+    file.close();
+    return false;
+}
+
+void list::getScientist ()
+{
+    name = _name;
+    sex = _sex;
+    birthYear = _birthYear;
+    deathYear = _deathYear;
+    TuringAwards = _TuringAwards;
+}
+
--- Vln-2016/list.h
+++ Vln-2016/list.h
@@ -1,20 +1,29 @@
 #ifndef LIST_H
 #define LIST_H
 #include <string>
+#include <fstream>
 using namespace std;
+
 
 class list
 {
 private:
-    string name;
-    int birthYear;
-    int deathYear;
-    char sex;
-    int TuringAwards;
+    string _name;
+    int _birthYear;
+    int _deathYear;
+    char _sex;
+    int _TuringAwards;
 
 public:
-    list();
+    string name;
+    char sex;
+    int birthYear;
+    int deathYear;
+    int TuringAwards;
 
+    list();
+    bool readFile ();
+    void getScientist ();
 
 };
 
