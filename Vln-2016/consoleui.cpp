a493d7d9cea9797edaf6e01acda5ea839d51c0a6
Stefán Hjartarson <stefanh@Stefans-MacBook-Air.local>
2016-12-05 11:37:44 +0000

    ToLower

--- Vln-2016/consoleui.cpp
+++ Vln-2016/consoleui.cpp
@@ -349,7 +349,7 @@ void consoleUI::run()
                 //here it prints the users that have the name that users wants to delete
                 for(int i = 0 ; i < scientist.getSize(); i++)
                 {
-                    if(name ==  scientist.changeToLower(scientist.getFirstNameFromList(i)) || name == scientist.changeToLower(scientist.getLastNameFromList(i)))
+                    if(scientist.changeToLower(name) ==  scientist.changeToLower(scientist.getFirstNameFromList(i)) || name == scientist.changeToLower(scientist.getLastNameFromList(i)))
                     {
                         string sex;
                         string isDead;
