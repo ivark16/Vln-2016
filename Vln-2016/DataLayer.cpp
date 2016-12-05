ac322b3c9afd057ee7e50a7b5597667623c20876
Ívar Örn Kristjánsson <var rn Kristjnsson>
2016-12-05 15:05:32 +0000

    Vid stebbi ad gera connection vid SQL fra dataLayer

--- Vln-2016/DataLayer.cpp
+++ Vln-2016/DataLayer.cpp
@@ -7,13 +7,18 @@
 
 ScientisListFromSQL::ScientisListFromSQL()
 {
-    QSqlDatabase db;
-    if(QSqlDatabase::contains("qt_sql_default_connection")) {
-            db = QSqlDatabase::database("qt_sql_default_connection");
-        } else {
-            db = QSqlDatabase::addDatabase("QSQLITE");
-            db.setDatabaseName("PersonCompSci.sqlite");
-            db.open();
-        }
+    QSqlDatabase m_db;
+
+       m_db = QSqlDatabase::addDatabase("QSQLITE");
+       m_db.setDatabaseName("Scientists.sqlite");
+
+       if (!m_db.open())
+       {
+          qDebug() << "Error: connection with database fail";
+       }
+       else
+       {
+          qDebug() << "Database: connection ok";
+       }
 }
 
