d2437354de54f7d3b39d44f0006ecba8567c7fa4
Dagmar <Dagmar>
2016-12-06 11:11:17 +0000

    Nicenicenicenicenicenicnei

--- Vln-2016/scientist.cpp
+++ Vln-2016/scientist.cpp
@@ -14,10 +14,25 @@ Scientist::Scientist(string fName, string lName, string nationality, int bYear, 
     _deathYear = dYear;
     _sex = gender;
     _awardYear = aYear;
+
+    DataLayer writer;
+    //writer.addNewScientist(fName, lName, nationality, bYear, dYear, gender, aYear); ADD THIS FUNCTION TO DATALAYER
 }
 
 void Scientist::readFile(vector<Scientist>& scientists)
 {
+    DataLayer reader;
+    /*  WRITE THESE FUNCTIONS TO DATALAYER
+    do(
+    _firstName = reader.getFirstName(i);
+    _lastName = reader.getLastName(i);
+    _nationality = reader.getNationality(i);
+    _birthYear = reader.getBirthYear(i);
+    _deathYear = reader.getDeathYear(i);
+    _sex = reader.getGender(i);
+    _awardYear = reader.getAwardYear(i);
+    ) while {reader.hasMoreScientists()}
+    */
 
 }
 string Scientist::getFirstName()
