007c3c6568ff8ff06d9c5f48333d476ac6a5ed39
Ívar Örn Kristjánsson <var rn Kristjnsson>
2016-12-12 16:13:03 +0000

    Eg og Anna ad bua til scientist table

--- Gui-Vln-2016/mainwindow.cpp
+++ Gui-Vln-2016/mainwindow.cpp
@@ -6,9 +6,55 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    displayAllScientists();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_tableViewScientist_clicked(const QModelIndex &index)
+{
+    //ui->button_remove_student->setEnabled(true);
+}
+
+void MainWindow::displayAllScientists()
+{
+    vector<Scientist> scientist = scientistService.displayScientist();
+    displayScientist(scientist);
+}
+
+void MainWindow::displayScientist(vector<Scientist> scientists)
+{
+    ui->tableWidget->clearContents();
+
+    ui->tableWidget->setRowCount(scientists.size());
+
+    for (unsigned int row = 0; row < scientists.size(); row++)
+    {
+        Scientist currentScientist = scientists[row];
+
+        char a = currentScientist.getGender();
+        string s(1, a);
+        QString firstName = QString::fromStdString(currentScientist.getFirstName());
+        QString lastName = QString::fromStdString(currentScientist.getLastName());
+        QString gender = QString::fromStdString(s);
+        //QString gender = QChar::QChar(currentScientist.getGender());
+        QString nationality = QString::fromStdString(currentScientist.getNationality());
+        QString birtYear = QString::number(currentScientist.getBirthYear());
+        QString deathYear = QString::number(currentScientist.getDeathYear());
+        QString awardYear = QString::number(currentScientist.getAwardYear());
+        QString ID = QString::number(currentScientist.getID());
+
+        ui->tableWidget->setItem(row, 0, new QTableWidgetItem(ID));
+        ui->tableWidget->setItem(row, 1,  new QTableWidgetItem(firstName));
+        ui->tableWidget->setItem(row, 2,  new QTableWidgetItem(lastName));
+        ui->tableWidget->setItem(row, 3,  new QTableWidgetItem(gender));
+        ui->tableWidget->setItem(row, 4,  new QTableWidgetItem(nationality));
+        ui->tableWidget->setItem(row, 5,  new QTableWidgetItem(birtYear));
+        ui->tableWidget->setItem(row, 6,  new QTableWidgetItem(deathYear));
+        ui->tableWidget->setItem(row, 7,  new QTableWidgetItem(awardYear));
+    }
+
 }
--- Gui-Vln-2016/mainwindow.h
+++ Gui-Vln-2016/mainwindow.h
@@ -1,7 +1,12 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "scientist.h"
+#include "listservices.h"
+#include <vector>
 #include <QMainWindow>
+#include <string>
+using namespace std;
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +20,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_tableViewScientist_clicked(const QModelIndex &index);
+
 private:
     Ui::MainWindow *ui;
+    void displayAllScientists();
+    void displayScientist (vector <Scientist> scientists);
+
+    listServices scientistService;
 };
 
 #endif // MAINWINDOW_H
