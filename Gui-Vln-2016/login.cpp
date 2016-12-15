ca61f126b269d903978110e2ed8af815a6d12e06
Stefán Hjartarson <stefanh@Stefans-MacBook-Air.local>
2016-12-15 21:23:04 +0000

    login með encryption done

--- Gui-Vln-2016/login.cpp
+++ Gui-Vln-2016/login.cpp
@@ -13,25 +13,6 @@ login::~login()
     delete ui;
 }
 
-int login::checkLogin()
-{
-    int _cl =1;
-    if ( (!ui->lineEditUser->text().isEmpty()) &&(!ui->lineEditPwd->text().isEmpty()))
-    {
-            if((ui->lineEditUser->text()=="admin") && (ui->lineEditPwd->text()=="1234"))
-            {
-                QMessageBox::about(this, tr("Login success"), tr("<p>Welcome...</p>"));
-                _cl = 0;
-            }
-            else
-            {
-                QMessageBox::about(this, tr("Login unsuccess"), tr("<p>Wrong password</p>"));
-                _cl = 1;
-            }
-     }
-     return _cl;
-}
-
 void login::on_pushButtonLogin_clicked()
 {
     vector<userandpass> checking;
@@ -48,12 +29,18 @@ void login::on_pushButtonLogin_clicked()
         QString getPwd = QString::fromStdString(checking[i].getpassword());
         if(getUser == username)
         {
-            if(getPwd == password)
+            QString convert;
+            string convertedKey;
+            string decrypt = getPwd.toLocal8Bit().constData();
+            convertedKey = theDecrypter(decrypt);
+            QString decryptedPwd = QString::fromStdString(convertedKey);
+            if(decryptedPwd == password)
             {
+                QDialog::accept();
                 QMessageBox popup;
                 popup.setText("Login successful");
                 popup.exec();
-                close();
+
             }
             else
             {
@@ -70,4 +57,71 @@ void login::on_pushButton_2_clicked()
 {
     _users.setModal(true);
     _users.exec();
+}
+
+string login::theDecrypter(string text)
+{
+    string decrypt = "";
+    int key = 21;
+    decrypt = deChiperText(text, key);
+    return decrypt;
+}
+
+string login::deChiperText(string text, int shift)
+{
+    string result = "";
+
+    for(unsigned int i = 0; i < text.length() ; i++)
+    {
+        char currentCharacter = text[i];
+
+        if(isUpperCase(currentCharacter) || isLowerCase(currentCharacter))
+        {
+            result += shiftCharacter(currentCharacter, shift);
+        }
+        else
+        {
+            result += currentCharacter;
+        }
+    }
+
+    return result;
+}
+
+char login::shiftCharacter(char character, int shift)
+{
+    int characterAsciiValue = static_cast<int>(character);
+    int characterAsciiOffset = 0;
+
+    if(isUpperCase(character))
+    {
+        characterAsciiOffset = 65;
+    }
+    else if(isLowerCase(character))
+    {
+        characterAsciiOffset = 97;
+    }
+
+    if(characterAsciiOffset)
+    {
+        characterAsciiValue -= characterAsciiOffset;
+        characterAsciiValue = (characterAsciiValue + shift) % 26;
+        characterAsciiValue += characterAsciiOffset;
+    }
+
+    return static_cast<char>(characterAsciiValue);
+}
+
+bool login::isUpperCase(char character)
+{
+    int characterInAsciiValure = static_cast<int>(character);
+
+    return (characterInAsciiValure >= 65 && characterInAsciiValure <= 90);
+}
+
+bool login::isLowerCase(char character)
+{
+    int characterInAsciiValure = static_cast<int>(character);
+
+    return (characterInAsciiValure >= 97 && characterInAsciiValure <= 122);
 }
--- Gui-Vln-2016/registeruser.cpp
+++ Gui-Vln-2016/registeruser.cpp
@@ -15,12 +15,22 @@ registeruser::~registeruser()
 
 void registeruser::on_pushButtonRegister_clicked()
 {
-    _username = ui->lineEditUserName->text().toStdString();
-    string originalText = ui->lineEditPassword->text().toStdString();
-    _password = chiperText(originalText, 5);
-    userandpass itemUser(_username, _password);
-    _lists.regiserUsers(itemUser);
-    close();
+    bool isLegitComputer;
+    bool hasUserName = getUserName();
+    bool hasPassWord = getPassWord();
+
+    isLegitComputer = (hasUserName && hasPassWord);
+
+    if(isLegitComputer)
+    {
+        _username = ui->lineEditUserName->text().toStdString();
+        string originalText = ui->lineEditPassword->text().toStdString();
+        _password = chiperText(originalText, 5);
+        userandpass itemUser(_username, _password);
+        _lists.regiserUsers(itemUser);
+        close();
+    }
+
 }
 
 string registeruser::chiperText(string text, int shift)
@@ -33,7 +43,7 @@ string registeruser::chiperText(string text, int shift)
 
         if(isUpperCase(currentCharacter) || isLowerCase(currentCharacter))
         {
-            result += shiftCharacter(currentCharacter, 5);
+            result += shiftCharacter(currentCharacter, shift);
         }
         else
         {
@@ -80,5 +90,100 @@ bool registeruser::isLowerCase(char character)
 {
     int characterInAsciiValure = static_cast<int>(character);
 
-    return (characterInAsciiValure >= 107 && characterInAsciiValure <= 122);
+    return (characterInAsciiValure >= 97 && characterInAsciiValure <= 122);
+}
+
+bool registeruser::getUserName()
+{
+    string potentialUSer = ui -> lineEditUserName ->text().toStdString();
+
+    bool hasLegalCharacters = true;
+    bool hasContent = true;
+
+    //Checks that there is something in the field
+    if((ui ->lineEditUserName->text()).isEmpty())
+    {
+        hasContent = false;
+    }
+
+    //check that it has only characters
+    for(unsigned int i = 0; i < potentialUSer.size(); i++)
+    {
+        if(isalpha(potentialUSer[i]) || isdigit(potentialUSer[i]) || potentialUSer[i] == ' ')
+        {
+            //do nothing
+        }
+        else
+        {
+            hasLegalCharacters = false;
+        }
+    }
+
+    //Throws errors if any are needed
+    if(!hasContent)
+    {
+        ui ->labelUser ->setText("<span style='color: red'>Input required</span>");
+
+    }
+    else if(!hasLegalCharacters)
+    {
+        ui ->labelUser ->setText("<span style='color: red'>Name can only contain characters, numbers and spaces</span>");
+    }
+    else if(potentialUSer.size() < 2 || potentialUSer.size() > 10)
+    {
+        ui -> labelUser ->setText("<span style=' color: red'>Name must be between 2 and 10 characters</span>");
+    }
+    else
+    {
+        //If there are no problems, the first name is legal.
+        _username = potentialUSer;
+    }
+    return (hasContent && hasLegalCharacters && !(potentialUSer.size() < 2 || potentialUSer.size() > 10));
+}
+
+bool registeruser::getPassWord()
+{
+    string potentialPassWord = ui -> lineEditPassword ->text().toStdString();
+
+    bool hasOnlyChar = true;
+    bool hasContent = true;
+
+    //Checks that there is something in the field
+    if((ui ->lineEditPassword->text()).isEmpty())
+    {
+        hasContent = false;
+    }
+
+    //check that it has only characters
+    for(unsigned int i = 0; i < potentialPassWord.size(); i++)
+    {
+        if(isalpha(potentialPassWord[i]))
+        {
+            //do nothing
+        }
+        else
+        {
+            hasOnlyChar = false;
+        }
+    }
+
+    //Throws errors if any are needed
+    if(!hasContent)
+    {
+        ui ->labelPwd->setText("<span style='color: red'>Last name required</span>");
+    }
+    else if(!hasOnlyChar)
+    {
+        ui ->labelPwd ->setText("<span style='color: red'>Name can only contain latin characters</span>");
+    }
+    else if(potentialPassWord.size() <= 3 || potentialPassWord.size() >= 10)
+    {
+        ui -> labelPwd ->setText("<span style=' color: red'>Name must be between 2 and 10 characters</span>");
+    }
+    else
+    {
+        //If there are no problems, the last name is legal.
+        _password = potentialPassWord;
+    }
+    return (hasContent && hasOnlyChar && !(potentialPassWord.size() <= 3 || potentialPassWord.size() >= 10));
 }
