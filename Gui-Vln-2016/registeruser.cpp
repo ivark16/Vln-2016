2ab586e4c1d5dc58a25ec9789d2ede37f4cdd366
Stefán Hjartarson <stefanh@Stefans-MacBook-Air.local>
2016-12-15 17:57:33 +0000

    encryption

--- Gui-Vln-2016/registeruser.cpp
+++ Gui-Vln-2016/registeruser.cpp
@@ -16,7 +16,8 @@ registeruser::~registeruser()
 void registeruser::on_pushButtonRegister_clicked()
 {
     _username = ui->lineEditUserName->text().toStdString();
-    _password = ui->lineEditPassword->text().toStdString();
+    string originalText = ui->lineEditPassword->text().toStdString();
+    _password = chiperText(originalText, 5);
     userandpass itemUser(_username, _password);
     _lists.regiserUsers(itemUser);
     close();
@@ -24,20 +25,60 @@ void registeruser::on_pushButtonRegister_clicked()
 
 string registeruser::chiperText(string text, int shift)
 {
+    string result = "";
 
+    for(unsigned int i = 0; i < text.length() ; i++)
+    {
+        char currentCharacter = text[i];
+
+        if(isUpperCase(currentCharacter) || isLowerCase(currentCharacter))
+        {
+            result += shiftCharacter(currentCharacter, 5);
+        }
+        else
+        {
+            result += currentCharacter;
+        }
+    }
+
+
+    return result;
 }
 
 char registeruser::shiftCharacter(char character, int shift)
 {
+    int characterAsciiValue = static_cast<int>(character);
+    int characterAsciiOffset = 0;
 
+    if(isUpperCase(character))
+    {
+        characterAsciiOffset = 65;
+    }
+    else if(isLowerCase(character))
+    {
+        characterAsciiOffset = 97;
+    }
+
+    if(characterAsciiOffset)
+    {
+        characterAsciiValue -= characterAsciiOffset;
+        characterAsciiValue = (characterAsciiValue + shift) % 26;
+        characterAsciiValue += characterAsciiOffset;
+    }
+
+    return static_cast<char>(characterAsciiValue);
 }
 
 bool registeruser::isUpperCase(char character)
 {
+    int characterInAsciiValure = static_cast<int>(character);
 
+    return (characterInAsciiValure >= 65 && characterInAsciiValure <= 90);
 }
 
 bool registeruser::isLowerCase(char character)
 {
+    int characterInAsciiValure = static_cast<int>(character);
 
+    return (characterInAsciiValure >= 107 && characterInAsciiValure <= 122);
 }
